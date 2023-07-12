873d8f33ed25a0d806cb4da5a2fb6305f694b82f
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-07-13 00:13:41 +0600

    updating data

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log("Meal Counter App is Running")
+    console.log("Meal Counter App is Running", port)
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.wpchpxk.mongodb.net/?retryWrites=true&w=majority`;
@@ -16,44 +16,55 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true } });
 
 async function run() {
+   
     try {
         await client.connect();
-        const AllCellDataCollections = client.db("Meal-Counter").collection("AllCellData")
-
-        app.get("/", (res, req) => {
-            console.log("Server is Running")
-        })
+        const AllCellDataCollections = client.db("Meal-Counter").collection("AllCellData");
 
         app.post("/addData", async (req, res) => {
             const AllCellData = await AllCellDataCollections.find().toArray();
             const BodyData = req.body;
+
+            const { name, day, cellData } = BodyData;
+            console.log(BodyData.data,"NameMatched")
+
             if (AllCellData.length) {
-                const { name, day, cellData } = BodyData;
-                const UserInfo = [name, day, cellData]
-                const Found = AllCellData.find(data => UserInfo.includes(data.name) && UserInfo.includes(data.day) && UserInfo.includes(data.cellData));
-
-                if (Found) {
-                    const update = { $set: { name, day, cellData } };
-                    const options = { upsert: true };
-                    const result = await collection.updateOne(filter, update, options);
-
-                    if (result.upsertedCount > 0) {
-                        console.log(result);
-                    } else {
-                        console.log(result);
+                // name checked
+                const NameMatched = AllCellData.find(item => item.name === name);
+               
+                if (NameMatched === undefined) {
+                    const dataToInsert = { name:name, info:[{ day, cellData }] };
+                    const AddResult = await AllCellDataCollections.insertOne(dataToInsert)
+                    return console.log(AddResult);
+                } else {
+                    // data and day checked
+                    const UserInfo = [name, day, cellData];
+                    const DayDataMatched = NameMatched.info.find(data => UserInfo.includes(data.day) && UserInfo.includes(data.cellData));
+                    
+                    if (DayDataMatched) {
+                        const result= await AllCellDataCollections.info.updateOne({...DayDataMatched,cellData})
+                        return console.log(result);
                     }
-                }
-                else {
-
+                    // day/cellData dosen't exist 
+                    const update = { $set: { day, cellData } };
+                    const options = { upsert: true };
+                    const result = await AllCellDataCollections.info.insertOne({ day, cellData }, update, options);
+                    console.log(result)
                 }
             } else {
-                const AddResult = await AllCellDataCollections.insertOne(BodyData)
-                return;
+                // If DataBase full empty at the first time 
+                const dataToInsert = { name:name, info:[{ day, cellData }] };
+                const AddResult = await AllCellDataCollections.insertOne(dataToInsert)
+                return console.log(AddResult);
             }
         })
 
     } finally {
     }
+    app.get("/", (res, req) => {
+        console.log("Running")
+      res.send("Server is running");
+    })
 }
 run().catch(console.dir);
 
