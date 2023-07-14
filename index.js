b3feafcd0c325c092cd10fe599f9e5eb24cc5e8d
TanjimulSabbir <tanjimulislamsabbir02@gmail.com>
2023-07-15 00:18:47 +0600

    updating data

--- index.js
+++ index.js
@@ -24,43 +24,49 @@ async function run() {
         app.post("/addData", async (req, res) => {
             const AllCellData = await AllCellDataCollections.find().toArray();
             const BodyData = req.body;
-
             const { name, day, cellData } = BodyData;
-            console.log(BodyData.data, "NameMatched")
-
+          
             if (AllCellData.length) {
-                // name checked
-                const NameMatched = AllCellData.find(item => item.name === name);
-
-                if (NameMatched === undefined) {
-                    const dataToInsert = { name: name, info: [{ day, cellData }] };
-                    const AddResult = await AllCellDataCollections.insertOne(dataToInsert)
-                    return console.log(AddResult);
-                } else {
-                    // data and day checked
-                    const filter = { name, "info.day": day };
-                    const update = { $set: { "info.$.cellData": cellData } };
-
-                    const result = await AllCellDataCollections.updateOne(filter, update);
-
-                    if (result.matchedCount > 0) {
-                        console.log('Document updated');
-                    } else {
-                        // If no matching document with the same day is found, add a new entry
-                        const addResult = await AllCellDataCollections.updateOne(
-                            { name },
-                            { $addToSet: { info: { day, cellData } } }
-                        );
-                        console.log('Document inserted');
-                        console.log(addResult)
-                    }
-                }
-            } else {
-                // If DataBase full empty at the first time 
+              // name checked
+              const NameMatched = AllCellData.find(item => item.name === name);
+          
+              if (NameMatched === undefined) {
                 const dataToInsert = { name: name, info: [{ day, cellData }] };
-                const AddResult = await AllCellDataCollections.insertOne(dataToInsert)
-                return console.log(AddResult);
+                const AddResult = await AllCellDataCollections.insertOne(dataToInsert);
+                return res.send(AddResult);
+              } else {
+                // data and day checked
+                const filter = { name, "info.day": day };
+                const update = { $set: { "info.$.cellData": cellData } };
+          
+                const result = await AllCellDataCollections.updateOne(filter, update);
+          
+                if (result.matchedCount > 0) {
+                  console.log('Document updated');
+                  return res.send(result);
+                } else {
+                  // If no matching document with the same day is found, add a new entry
+                  const addResult = await AllCellDataCollections.updateOne(
+                    { name },
+                    { $addToSet: { info: { day, cellData } } }
+                  );
+                  console.log('Document inserted');
+                  console.log(addResult);
+                  return res.send(addResult);
+                }
+              }
+            } else {
+              // If DataBase is empty at the first time
+              const dataToInsert = { name: name, info: [{ day, cellData }] };
+              const AddResult = await AllCellDataCollections.insertOne(dataToInsert);
+              return res.send(AddResult);
             }
+          });
+          
+
+        app.get("/getData",async(req,res)=>{
+            const result= await AllCellDataCollections.find().toArray();
+            res.send(result);
         })
 
     } finally {
